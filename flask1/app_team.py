21b13dd5db79924d40521151e1450aa60103c742
JHJ20 <jin010630@dgu.ac.kr>
2024-11-05 23:15:00 +0900

    성별, 언어 삭제(오류원인) && 사진 추가

--- flask1/app_team.py
+++ flask1/app_team.py
@@ -2,39 +2,40 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# 최초 메인 페이지를 보여주는 루트 경로
+student_images = {
+    "2020110401": "https://raw.githubusercontent.com/CSID-DGU/2024-2-OSSPrac-azaping-01/main/pic/2020110401.jpg",
+    "20230002": "https://raw.githubusercontent.com/CSID-DGU/2024-2-OSSPrac-azaping-01/main/pic/student2.jpg",
+    "20230003": "https://raw.githubusercontent.com/CSID-DGU/2024-2-OSSPrac-azaping-01/main/pic/student3.jpg",
+    # 다른 학번에 따른 URL 추가 가능
+}
+
 @app.route('/')
 def index():
-    return render_template('app_index.html')  # 위 HTML 코드를 form.html 파일로 저장해야 합니다
-
-# 학생 정보를 입력하는 경로
+    return render_template('app_index.html')
 
 @app.route('/input')
 def input():
     return render_template('app_input.html')
 
-# 제출된 데이터를 처리하여 출력하는 경로
 @app.route('/result', methods=['POST'])
 def result():
-    # form 데이터 가져오기
+    # 수집된 데이터를 확인하기 위해 각 필드를 출력합니다.
     names = request.form.getlist('name[]')
+    departments = request.form.getlist('department[]')
     student_numbers = request.form.getlist('StudentNumber[]')
     parts = request.form.getlist('part[]')
-    genders = request.form.getlist('gender[]')
     emails = request.form.getlist('email[]')
     mbtis = request.form.getlist('mbti[]')
 
-    # 컴퓨터 언어는 체크박스 배열로 수집
-    languages_all = request.form.getlist('languages[]')
-    languages_per_person = [languages_all[i:i+5] for i in range(0, len(languages_all), 5)]
+    images = [
+        student_images.get(student_number, "https://raw.githubusercontent.com/CSID-DGU/2024-2-OSSPrac-azaping-01/main/pic/default.jpg")
+        for student_number in student_numbers
+    ]
 
-    # 데이터 템플릿으로 전달
-    students = zip(names, student_numbers, parts, genders, languages_per_person, emails, mbtis)
+
+    # 학생 정보를 zip으로 묶어 템플릿에 전달
+    students = zip(names, departments, student_numbers, parts, emails, mbtis, images)
     return render_template('app_result.html', students=students)
-
-@app.route('/contact')
-def contact_info():
-    return render_template('app_contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
